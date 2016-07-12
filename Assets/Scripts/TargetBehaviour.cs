cfc7db43d5f4e31eacc5a8298bdd36554d111621
Scott Bromander <scott@primeacademy.io>
2016-07-11 21:13:29 -0500

    adds target

---
+++ Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetBehaviour : MonoBehaviour {
+
+	private bool beenHit = false;
+	private Animator animator;
+	private GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		parent = transform.parent.gameObject;
+		animator = parent.GetComponent<Animator> ();
+	}
+
+	/// <summary>
+	/// Called whenever the player clicks the object. Only works if you have a collider on the object.
+	/// </summary>
+	void OnMouseDown(){
+		if (!beenHit) {
+			beenHit = true;
+			animator.Play ("Flip");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
