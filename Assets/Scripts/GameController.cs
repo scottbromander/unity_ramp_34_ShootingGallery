be4233f751e2968e49c359206d623d6f8ecdea8d
Scott Bromander <scott@primeacademy.io>
2016-07-14 22:36:23 -0500

    add time ui element

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,20 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 	public static GameController _instance;
+
+	private float timeLeft;
+	public Text timeText;
 
 	[HideInInspector]
 	public List<TargetBehaviour> targets = new List<TargetBehaviour>();
 
 	void Awake() {
 		_instance = this;
+
+		timeLeft = 50;
+		timeText.text = timeLeft.ToString ();
 	}
 
 	// Use this for initialization
 	void Start () {
+
+		iTween.ValueTo (gameObject, iTween.Hash (
+			"from", timeLeft,
+			"to", 0,
+			"time", timeLeft,
+			"onupdatetarget", gameObject,
+			"onupdate", "tweenUpdate",
+			"oncomplete", "GameComplete"
+		));
+
 		StartCoroutine ("SpawnTargets");
+	}
+
+	void GameComplete(){
+		StopCoroutine ("SpawnTargets");
+		timeText.color = Color.black;
+		timeText.text = "GAME OVER";
+	}
+
+	void TweenUpdate(float newValue){
+		timeLeft = newValue;
+		if (timeLeft > 10) {
+			timeText.text = timeLeft.ToString("#");
+		} else {
+			timeText.color = Color.red;
+			timeText.text = timeLeft.ToString ("#.0");
+		}
 	}
 
 	void SpawnTarget(){
